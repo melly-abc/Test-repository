1f81d1d91db19f107ff13be74d9f94e1b4d1e3c7
Tae <hotgenmai@gmail.com>
2018-06-08 23:25:00 +0900

    corrected

--- pi2.py
+++ pi2.py
@@ -1,14 +1,16 @@
 #coding:utf-8
 # Leibniz formula
+if __name__=="__main__":
+ import sys
+ 
+ pi=0.0
 
-pi=0.0
+ sys.stdout.write('Input the number of iterations\n>n=')
 
-sys.stdout.write('Input the number of iterations\n>n=')
-
-for i in range(int(raw_input())):
+ for i in range(int(raw_input())):
         a=1.0
         for j in range(0,i):
                 a=a*(-1.0)
         pi=pi+a/(2.0*float(i)+1.0)
 
-print (4.0*pi)
+ print (4.0*pi)
