dcbc83f5d9a3b89e71649208cef6639e7ee20e03
Tae <hotgenmai@gmail.com>
2018-06-09 12:35:42 +0900

    corrected

--- pi2.py
+++ pi2.py
@@ -9,8 +9,8 @@ if __name__=="__main__":
 
  for i in range(int(raw_input())):
         a=1.0
-        for j in range(0,i):
-                a=a*(-1.0)
-        pi=pi+a/(2.0*float(i)+1.0)
+        for j in range(i):
+                a*=(-1.0)
+        pi+=a/(2.0*float(i)+1.0)
 
  print (4.0*pi)
